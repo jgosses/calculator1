771d7557b9909b419130342b19e771d48fc0ee8a
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-30 23:37:18 -0400

    fix js layout issue

--- js/main.js
+++ js/main.js
@@ -9,34 +9,32 @@ const keys = calculator.querySelector('.calculator__keys')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        // Do Something
+        const key = e.target
+        const action = key.dataset.action
+        
+        if (!action) {
+            console.log('number key!')
+        }
+        
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        )   {
+            console.log('operator key!')
+        }
+        
+        if (action === 'decimal') {
+            console.log('decimal key!')
+        }
+        
+        if (action === 'clear') {
+            console.log('clear key!')
+        }
+        
+        if (action === 'calculate') {
+            console.log('equal key!')
+        }
     }
 })
-
-const key = e.target
-const action = key.dataset.action
-
-if (!action) {
-    console.log('number key!')
-}
-
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-)   {
-    console.log('operator key!')
-}
-
-if (action === 'decimal') {
-    console.log('decimal key!')
-}
-
-if (action === 'clear') {
-    console.log('clear key!')
-}
-
-if (action === 'calculate') {
-    console.log('equal key!')
-}
